2f979c585fa6367ca5343bf511e967112fa9ba2a
Francisco Melo <aluno23155@ipt.pt>
2022-04-05 15:22:52 +0100

    Vets

--- Veterinarios/Data/ApplicationDbContext.cs
+++ Veterinarios/Data/ApplicationDbContext.cs
@@ -3,7 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Veterinarios.Models;
 
 namespace Veterinarios.Data {
+    /// <summary>
+    /// thos class conects outcode with database
+    /// </summary>
     public class ApplicationDbContext : IdentityDbContext {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options) {
         }
--- Veterinarios/Models/Vet.cs
+++ Veterinarios/Models/Vet.cs
@@ -1,5 +1,9 @@
-﻿namespace Veterinarios.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinarios.Models {
+    /// <summary>
+    /// data from Vets
+    /// </summary>
     public class Vet {
 
         public Vet()
@@ -7,10 +11,25 @@
             Appointment = new HashSet<Appointment>();
         }
 
-
+        /// <summary>
+        /// PK for vets
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Name of vet
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Name of file that has the vet photo
+        /// </summary>
         public string Photo { get; set; }
+
+        /// <summary>
+        /// Professional license of vet
+        /// </summary>
+        [Display(Name = "Professional License")]
         public string ProfessionalLicense { get; set; }
 
         public ICollection<Appointment> Appointment { get; set; }
